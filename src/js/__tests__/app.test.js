acd97c39ecc414782c778d2398719276c846524a
Yuri Shornikov <dbonlyfun@gmail.com>
2024-02-09 10:55:00 +0300

    correct thThrow

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,19 +1,19 @@
 import validateNumber from "../app";
 
 describe('test func', () => {
-    test('test number with 11 symbols', () => {
+    test('test number with 11 symbols for Russia', () => {
         const num = validateNumber('+7 968 000 00 00');
         expect(num).toBe('+79680000000');
     })
-    test('test number with 10 symbols', () => {
+    test('test number with 10 symbols for mistake', () => {
         const num = validateNumber(' (927) 000-00-00');
         expect(num).toBe('+79270000000');
     })
-    test('test number with 12 symbols', () => {
+    test('test number with 12 symbols for China', () => {
         const num = validateNumber('+86 000 000 0000');
         expect(num).toBe('+860000000000');
     })
-    // test('test number with less 10 symbols', () => {
-    //     expect(() => validateNumber('+86 000 000 ')).toThrowError('Uncorrect number phone');
-    // })
+    test('test number with less 10 symbols', () => {
+        expect(() => validateNumber('+86 000 000 ')).toThrow('Uncorrect number phone');
+    })
 })
--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,30 @@
 export default function validateNumber(phoneNumber) {
+// function validateNumber(phoneNumber) {
     // удаляем все лишнее
     const deleteSymbol = phoneNumber.replace(/\D/g, '');
 
-    let correctForm;
+    console.log(`deleteSymbol: ${deleteSymbol}`);
+    console.log(`deleteSymbol.length: ${deleteSymbol.length}`);
 
-    if (deleteSymbol.length == 12 || deleteSymbol.startsWith('86')) {
+    let correctForm = null;
+
+    if (deleteSymbol.length == 12 && deleteSymbol.startsWith('86')) {
+        console.log(deleteSymbol)
         correctForm = '+8' + deleteSymbol.slice(1);
     } else if (deleteSymbol.length == 10) {
         correctForm = '+7' + deleteSymbol;
     } else if (deleteSymbol.length == 11) {
         correctForm = '+7' + deleteSymbol.slice(1);
-    } 
-    // else {
-    //     console.log('Uncorrect number phone')
-        
-    // }
+    }
+    else {
+        throw new Error('Uncorrect number phone');
+    }
     return correctForm;
 }
+
+// try {
+//     const phonenum = validateNumber('+86 000 000 ');
+//     console.log(phonenum);
+// } catch (error) {
+//     console.error(error.message);
+// }
